e0fe26d0ad7686e49e9d8b6c43fee6a42091f18a
JadeCathleen <cormier.jade@gmail.com>
2022-04-07 11:27:11 +0200

    add tests to check validity of user - all passing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Test to check the validity preferences for users
+  test "invalid if it has no email" do
+    user = User.new(password: "123456")
+    user.valid?
+    assert_not user.errors[:email].empty?
+  end
+
+  test "invalid if it has no password" do
+    user = User.new(email: "test@test.com")
+    user.valid?
+    assert_not user.errors[:password].empty?
+  end
+
+  test "invalid if the password is shorter than 6 characters" do
+    user = User.new(email: "test@test.com", password: "123")
+    user.valid?
+    assert_not user.errors[:password].empty?
+  end
+
+  test "email is unique for each user" do
+    User.create(email: "test@test.com", password: "123456")
+    user = User.new(email: "test@test.com", password: "123457")
+    user.valid?
+    assert_not user.errors[:email].empty?
+  end
 end
