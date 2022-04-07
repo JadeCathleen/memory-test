bd17b72bf7fde22cfc830c34b26d64f86d63e810
JadeCathleen <cormier.jade@gmail.com>
2022-04-07 12:31:03 +0200

    modify order unit test to match new db schema

--- test/models/order_product_test.rb
+++ test/models/order_product_test.rb
@@ -3,7 +3,9 @@ require 'test_helper'
 class OrderProductTest < ActiveSupport::TestCase
   # Check the presence of each columns in OrderProducts Table
   test "invalid if it has no product quantity" do
-    order_product = OrderProduct.new(order: orders(:one), product: products(:one))
+    order = Order.new(order_code: 'test',user: users(:regular), customer: customers(:one), quantity: 2, order_date: Time.now)
+    order_product = OrderProduct.new(order: order, product: products(:one))
+
     order_product.valid?
     assert_not order_product.errors[:product_quantity].empty?
   end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -3,25 +3,25 @@ require 'test_helper'
 class OrderTest < ActiveSupport::TestCase
   # Check the presence of each columns of Orders Table
   test "invalid if it has no order_code" do
-    order = Order.new(user: users(:regular), customer: customers(:one), product: products(:one), quantity: 2, order_date: Time.now)
+    order = Order.new(user: users(:regular), customer: customers(:one), order_date: Time.now)
     order.valid?
     assert_not order.errors[:order_code].empty?
   end
 
   test "invalid if it has no order_date" do
-    order = Order.new(user: users(:regular), customer: customers(:one), product: products(:one), quantity: 2, order_code: 'test')
+    order = Order.new(user: users(:regular), customer: customers(:one), order_code: 'test')
     order.valid?
     assert_not order.errors[:order_date].empty?
   end
 
   test "invalid if it has no user attached" do
-    order = Order.new(order_code: 'test', customer: customers(:one), product: products(:one), quantity: 2, order_date: Time.now)
+    order = Order.new(order_code: 'test', customer: customers(:one), order_date: Time.now)
     order.valid?
     assert_not order.errors[:user].empty?
   end
 
   test "invalid if it has no customer attached" do
-    order = Order.new(order_code: 'test', user: users(:regular), product: products(:one), quantity: 2, order_date: Time.now)
+    order = Order.new(order_code: 'test', user: users(:regular), order_date: Time.now)
     order.valid?
     assert_not order.errors[:customer].empty?
   end
