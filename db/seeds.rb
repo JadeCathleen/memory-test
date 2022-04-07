b061865b3df2c088deec32c442a2f751bff3a7c9
JadeCathleen <cormier.jade@gmail.com>
2022-04-07 18:35:20 +0200

    relaunching seeds only for orders and orders products to solve problem of non uniqueness of order_code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,47 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 require 'csv'
 
-# if Rails.env.development?
-#   puts "Cleaning database..."
-#   OrderProduct.destroy_all
-#   Order.destroy_all
-#   User.destroy_all
-#   Customer.destroy_all
-#   Product.destroy_all
-# end
-# user = User.create(email: 'test@test.com', password: '123456')
+if Rails.env.development?
+  puts "Cleaning database..."
+  # OrderProduct.destroy_all
+  Order.destroy_all
+  # User.destroy_all
+  # Customer.destroy_all
+  # Product.destroy_all
+  puts 'Cleaning finished'
+end
+user = User.create(email: 'test@test.com', password: '123456')
 
 csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
-# Recipes seed
 
 data_filepath = 'lib/seeds/memory-tech-challenge-data.csv'
-# i = 0
-# initial_time = Time.now
+i = 0
+initial_time = Time.now
 CSV.foreach(Rails.root.join(data_filepath), csv_options) do |row|
-  # i += 1
-  # # Create the customer - it will only be saved if customer code (customer_id in CSV) does not exist yet (validity unit test)
-  # customer = Customer.where(customer_code: row['customer_id']).first
+  i += 1
+  # Create the customer - it will only be saved if customer code (customer_id in CSV) does not exist yet (validity unit test)
+  customer = Customer.where(customer_code: row['customer_id']).first
   # customer = Customer.create(customer_code: row['customer_id'], country: row['country']) if customer.nil?
-  p row['product_code']
-  # # Create the product - it will only be saved if product code does not exist yet (validity unit test)
-  # product = Product.where(product_code: row['product_code']).first
-  # # product = Product.create(product_code: row['product_code'], product_description: row['product_description'], price: row['unit_price']) if product.nil?
 
-  # # # Create the order - it will only be saved if order code (order_id in CSV) does not exist yet (validity unit test)
-  # order = Order.where(order_code: row['order_code']).first
-  # p order
-  # # order = Order.create(order_code: row['order_id'], order_date: Time.new(row['date']), customer: customer, user: user) if order.nil?
+  # Create the product - it will only be saved if product code does not exist yet (validity unit test)
+  product = Product.where(product_code: row['product_code']).first
+  # product = Product.create(product_code: row['product_code'], product_description: row['product_description'], price: row['unit_price']) if product.nil?
 
-  # # Associate the order with the product - OrderProduct table
-  # order_product = OrderProduct.create(order: order, product: product, product_quantity: row['quantity'])
-  # puts "Add product ##{order_product.product.product_code} to order ##{order_product.order.order_code}" if order_product.save!
+  # # Create the order - it will only be saved if order code (order_id in CSV) does not exist yet (validity unit test)
+  order = Order.where(order_code: row['order_code']).first
+  order = Order.create(order_code: row['order_id'], order_date: Time.new(row['date']), customer: customer, user: user) if order.nil?
 
-  # puts "Reached line #{i} in #{Time.now - initial_time}" if (i % 1000).zero?
+  # Associate the order with the product - OrderProduct table
+  order_product = OrderProduct.create(order: order, product: product, product_quantity: row['quantity'])
+  puts "Add product ##{order_product.product.product_code} to order ##{order_product.order.order_code}" if order_product.save!
+
+  puts "Reached line #{i} in #{Time.now - initial_time}" if (i % 1000).zero?
 end
