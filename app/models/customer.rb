96950b76ad36e72259b7f8feeaffd4f824297fec
JadeCathleen <cormier.jade@gmail.com>
2022-04-07 11:46:18 +0200

    add unit tests for product to check its validity - all passing

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < ApplicationRecord
+  has_many :orders
+
+  validates :country, :customer_code, presence: true
+  validates :customer_code, uniqueness: true
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Check the presence of each columns of Customer Table
+  test "invalid if it has no customer_code" do
+    customer = Customer.new(country: "France")
+    customer.valid?
+    assert_not customer.errors[:customer_code].empty?
+  end
+
+  test "invalid if it has no country" do
+    customer = Customer.new(customer_code: "test")
+    customer.valid?
+    assert_not customer.errors[:country].empty?
+  end
+
+  # Check the uniqueness of customer_code
+  test "customer_code is unique for each product" do
+    Customer.create(customer_code: "test", country: "France")
+    customer = Customer.new(customer_code: "test", country: "UK")
+    customer.valid?
+    assert_not customer.errors[:cutomer_code].empty?
+  end
 end
