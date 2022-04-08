040e6e7515c70adabd20d7ea8fe9b86341dd6a6e
JadeCathleen <cormier.jade@gmail.com>
2022-04-08 12:36:51 +0200

    add summary for all orders ok

--- app/controllers/order_products_controller.rb
+++ app/controllers/order_products_controller.rb
@@ -1,8 +1,11 @@
 class OrderProductsController < ApplicationController
 
   def index
-    # @orders = Order.where(user: current_user)
-    # @order_products = OrderProduct.where(order: Order.where(user: current_user))
-    @order_products = OrderProduct.all
+    @total_revenue = 0
+    OrderProduct.all.each do |order_product|
+      @total_revenue += order_product.revenue
+    end
+    @average_by_order = @total_revenue / Order.count
+    @customer_count = Customer.count
   end
 end
