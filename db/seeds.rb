bc97975676f7b88800e1a48159ef08dd3ccdafe3
JadeCathleen <cormier.jade@gmail.com>
2022-04-08 11:56:06 +0200

    resume work + seeds modified and ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,17 @@
 require 'csv'
 
-if Rails.env.development?
-  puts "Cleaning database..."
-  # OrderProduct.destroy_all
-  Order.destroy_all
-  # User.destroy_all
-  # Customer.destroy_all
-  # Product.destroy_all
-  puts 'Cleaning finished'
-end
-user = User.create(email: 'test@test.com', password: '123456')
+# if Rails.env.development?
+#   puts "Cleaning database..."
+#   # OrderProduct.destroy_all
+#   Order.destroy_all
+#   # User.destroy_all
+#   # Customer.destroy_all
+#   # Product.destroy_all
+#   puts 'Cleaning finished'
+# end
+# user = User.create(email: 'test@test.com', password: '123456')
+user = User.where(email: 'test@test.com').first
+
 
 csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
 
@@ -20,19 +22,24 @@ CSV.foreach(Rails.root.join(data_filepath), csv_options) do |row|
   i += 1
   # Create the customer - it will only be saved if customer code (customer_id in CSV) does not exist yet (validity unit test)
   customer = Customer.where(customer_code: row['customer_id']).first
-  # customer = Customer.create(customer_code: row['customer_id'], country: row['country']) if customer.nil?
+  customer = Customer.create(customer_code: row['customer_id'], country: row['country']) if customer.nil?
 
   # Create the product - it will only be saved if product code does not exist yet (validity unit test)
   product = Product.where(product_code: row['product_code']).first
-  # product = Product.create(product_code: row['product_code'], product_description: row['product_description'], price: row['unit_price']) if product.nil?
+  product = Product.create(product_code: row['product_code'], product_description: row['product_description'], price: row['unit_price']) if product.nil?
 
   # # Create the order - it will only be saved if order code (order_id in CSV) does not exist yet (validity unit test)
-  order = Order.where(order_code: row['order_code']).first
+  order = Order.where(order_code: row['order_id']).first
   order = Order.create(order_code: row['order_id'], order_date: Time.new(row['date']), customer: customer, user: user) if order.nil?
 
   # Associate the order with the product - OrderProduct table
-  order_product = OrderProduct.create(order: order, product: product, product_quantity: row['quantity'])
-  puts "Add product ##{order_product.product.product_code} to order ##{order_product.order.order_code}" if order_product.save!
+  order_product = OrderProduct.where(order: order, product: product).first
+  if order_product.nil?
+    order_product = OrderProduct.new(order: order, product: product, product_quantity: row['quantity'])
+    if order_product.save!
+      puts "Add product ##{order_product.product.product_code} to order ##{order_product.order.order_code}"
+    end
+  end
 
   puts "Reached line #{i} in #{Time.now - initial_time}" if (i % 1000).zero?
 end
