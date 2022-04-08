ae54239b788a29b3ef35037f6e8f5dda15c05c89
JadeCathleen <cormier.jade@gmail.com>
2022-04-08 14:15:30 +0200

    still on break - redo seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,15 @@
 require 'csv'
 
-# if Rails.env.development?
-#   puts "Cleaning database..."
-#   # OrderProduct.destroy_all
-#   Order.destroy_all
-#   # User.destroy_all
-#   # Customer.destroy_all
-#   # Product.destroy_all
-#   puts 'Cleaning finished'
-# end
-# user = User.create(email: 'test@test.com', password: '123456')
-user = User.where(email: 'test@test.com').first
+if Rails.env.development?
+  puts "Cleaning database..."
+  OrderProduct.destroy_all
+  Order.destroy_all
+  User.destroy_all
+  Customer.destroy_all
+  Product.destroy_all
+  puts 'Cleaning finished'
+end
+user = User.create(email: 'test@test.com', password: '123456')
 
 
 csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
@@ -28,18 +27,14 @@ CSV.foreach(Rails.root.join(data_filepath), csv_options) do |row|
   product = Product.where(product_code: row['product_code']).first
   product = Product.create(product_code: row['product_code'], product_description: row['product_description'], price: row['unit_price']) if product.nil?
 
-  # # Create the order - it will only be saved if order code (order_id in CSV) does not exist yet (validity unit test)
+  # Create the order - it will only be saved if order code (order_id in CSV) does not exist yet (validity unit test)
   order = Order.where(order_code: row['order_id']).first
   order = Order.create(order_code: row['order_id'], order_date: Time.new(row['date']), customer: customer, user: user) if order.nil?
 
   # Associate the order with the product - OrderProduct table
   order_product = OrderProduct.where(order: order, product: product).first
-  if order_product.nil?
-    order_product = OrderProduct.new(order: order, product: product, product_quantity: row['quantity'])
-    if order_product.save!
-      puts "Add product ##{order_product.product.product_code} to order ##{order_product.order.order_code}"
-    end
-  end
+
+  OrderProduct.create(order: order, product: product, product_quantity: row['quantity']) if order_product.nil?
 
   puts "Reached line #{i} in #{Time.now - initial_time}" if (i % 1000).zero?
 end
