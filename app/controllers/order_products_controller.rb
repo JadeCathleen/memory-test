c481e6a7ffe6625100a0b0a373a752cc50eb1fcd
JadeCathleen <cormier.jade@gmail.com>
2022-04-10 14:54:22 +0200

    add country selector

--- app/controllers/order_products_controller.rb
+++ app/controllers/order_products_controller.rb
@@ -3,10 +3,14 @@ class OrderProductsController < ApplicationController
   def index
     @order_products = policy_scope(OrderProduct).includes(:order, :product)
     @total_revenue = 0
+    @countries = []
     @order_products.each do |order_product|
       @total_revenue += order_product.revenue
     end
     @average_by_order = @total_revenue / Order.count
     @customer_count = Customer.count
+    @orders = policy_scope(Order).includes(:customer)
+    @orders.each {|order| @countries << order.customer.country}
+    @countries = @countries.uniq
   end
 end
